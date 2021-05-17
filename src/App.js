3b11b311b2d7c45a957c1f67e6fb1bce2a569846
Achrafelayedi <Achraf-externe.EL-AYEDI@cnr.tm.fr>
2021-05-17 11:05:37 +0200

    data noeud branche modele

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 //import logo from './logo.svg';
 //import react from "react";
 //import {MapContainer, TileLayer} from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from "react-leaflet";
 import "./App.css";
-//import Mymap from "./components/Mymap";
+import Mymap from "./components/Mymap";
 import Macarte from "./composantes/Macarte";
 
 function App() {
   return (
     <div className="App">
       <Macarte />
-      {/* <MapContainer center={[45.6936, 5.52826]} zoom={13}>
-          <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+      {/*  <MapContainer center={[45.6936, 5.52826]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
       </MapContainer> */}
     </div>
   );
--- src/composantes/Macarte.js
+++ src/composantes/Macarte.js
@@ -3,6 +3,17 @@ import L from "leaflet";
 import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from "react-leaflet";
 import noeud from "./../data/Noeud.json";
 import noeud_image from "./../img/noeud.png";
+import modele from "./../data/Modele_sault_brenaz.json";
+import branche from "./../data/Branche.json";
+
+var OpenStreetMap_France = L.tileLayer(
+  "https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
+  {
+    maxZoom: 20,
+    attribution:
+      '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
 
 function Macarte() {
   console.log(noeud);
@@ -10,6 +21,81 @@ function Macarte() {
     iconUrl: noeud_image,
     iconSize: [10, 10],
   });
+
+  function modele_style() {
+    return {
+      fillColor: "transparent",
+      color: "black",
+    };
+  }
+
+  function branche_style() {
+    return {
+      fillColor: "transparent",
+      color: "orange",
+    };
+  }
+
+  function onEachModele(modele, lay) {
+    lay.bindPopup(
+      "<p>NOM : " +
+        modele.properties.NOM +
+        "</p><p>Bief : " +
+        modele.properties.Bief +
+        "</p><p>Chute : " +
+        modele.properties.Chute +
+        "</p><p>MOA : " +
+        modele.properties.MOA +
+        "</p><p>Obj_modele : " +
+        modele.properties.Obj_modele +
+        "</p><p>Borne_debi : " +
+        modele.properties.Borne_debi +
+        "</p><p>Borne_de_1 : " +
+        modele.properties.Borne_de_1 +
+        "</p><p>Proj_refer : " +
+        modele.properties.Proj_refer +
+        "</p><p>Nivellement : " +
+        modele.properties.Nivellemen +
+        "</p><p>Modelisate : " +
+        modele.properties.Modelisate +
+        "</p><p>Dessinateu : " +
+        modele.properties.Dessinateu +
+        "</p><p>Date_saisi : " +
+        modele.properties.Date_saisi +
+        "</p><p>Valid : " +
+        modele.properties.Valid +
+        "</p><p>Shape_Leng : " +
+        modele.properties.Shape_Leng +
+        "</p><p>Shape_Area : " +
+        modele.properties.Shape_Area +
+        "</p>"
+    );
+  }
+
+  function onEachBranche(branche, lay) {
+    lay.bindPopup(
+      "<table><thead><tr><th>Branches de Sault-Brénaz</th></tr></thead><tbody></table><tbody><tr><td>Nom modèle : </td><td>" +
+        branche.properties.Nom_modele +
+        "<br></td></tr><tr><td>Type branche : </td><td>" +
+        branche.properties.Type_branc +
+        "</td></tr></tbody><br></td></tr><tr><td>Longueur : </td><td>" +
+        branche.properties.Longueur +
+        "</td></tr></tbody><br></td></tr><tr><td>Section_am : </td><td>" +
+        branche.properties.Section_am +
+        "</td></tr></tbody><br></td></tr><tr><td>Shape_Leng : </td><td>" +
+        branche.properties.Shape_Leng +
+        "</td></tr></tbody><br></td></tr><tr><td>Enabled : </td><td>" +
+        branche.properties.Enabled +
+        "</td></tr></tbody><br></td></tr><tr><td>NOM : </td><td>" +
+        branche.properties.NOM +
+        "</td></tr></tbody><br></td></tr><tr><td>Nom_noeud_ :</td><td>" +
+        branche.properties.Nom_noeud_ +
+        "</td></tr></tbody><br></td></tr><tr><td>Nom_noeud1 : </td><td>" +
+        branche.properties.Nom_noeud1 +
+        "</td></tr></tbody>"
+    );
+  }
+
   //noeud_geojson.addTo(map)
   /*   const iconNoeud = new L.Icon({
     iconUrl: require("./../img/noeud.png"),
@@ -37,6 +123,16 @@ function Macarte() {
           </Popup>
         </Marker>
       ))}
+      <GeoJSON
+        style={branche_style}
+        data={branche.features}
+        onEachFeature={onEachBranche}
+      />
+      <GeoJSON
+        style={modele_style}
+        data={modele.features}
+        onEachFeature={onEachModele}
+      />
     </MapContainer>
   );
 }
